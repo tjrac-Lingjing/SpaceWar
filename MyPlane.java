60c42a9402e104cc348b7a00d1a5d0c0520d3184
6017203206 <56570727+6017203206@users.noreply.github.com>
2019-12-30 11:27:45 +0800

    Add files via upload

--- MyPlane.java
+++ MyPlane.java
@@ -1,7 +1,4 @@
 package spacewar.detail;
-
-
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
@@ -17,6 +14,7 @@ import javax.swing.JPanel;
 
 import spacewar.utils.ImageUtil;
 
+
 public class MyPlane extends GameObject {
 
 	public static final int PLANE_WIDTH = 120;
@@ -28,36 +26,70 @@ public class MyPlane extends GameObject {
 	public static final int PLANE_X = 400;
 	public static final int PLANE_Y = 300;
 
+	public static List<BufferedImage> images = new ArrayList<BufferedImage>();// 存储升级战机图片
 	public static List<BufferedImage> images1 = new ArrayList<BufferedImage>();// 存储未升级战机图片
-
- 
+	public static List<BufferedImage> imagespro = new ArrayList<BufferedImage>();// 存储防护罩图片
+	public boolean isUpdate;// 标记要显示的战机图片下标索引，和战机是否升级相关
 	public int progress;
 
 	public MyPlane(boolean isUpdate) {
 		super(0, 0);
-	 
+		this.isUpdate = isUpdate;
+		// 战机的初始位置
 		point.x = PLANE_X;
 		point.y = PLANE_Y;
 		progress = 0;
 	}
-
+ 
 	@Override
-	public boolean draw(Graphics g, JPanel panel) {
-		
+	public boolean draw(Graphics g, JPanel panel, boolean pause) {
+		if (!pause) {
+			if (isUpdate) {
+				if (progress < 14) {
+					g.drawImage(images.get(progress), point.x, point.y, panel);
+					progress++;
+				} else {
+					progress = 0;
+					g.drawImage(images.get(progress), point.x, point.y, panel);
+				}
+			} else {
 				g.drawImage(images1.get(3), point.x, point.y, panel);
+			}
 			return true;
-		
+		}
+		return false;
+	}
+
+	public boolean draw(Graphics g, JPanel panel, boolean pause,boolean isProtect) {
+		if (!pause) {
+			if (isProtect) {
+				Point point = new Point(this.point.x - 25, this.point.y - 30);
+				g.drawImage(imagespro.get(0), point.x, point.y, panel);
+			}
+			if (isUpdate) {
+				if (progress < 14) {//十四个升级图片来回换形成升级的动画
+					g.drawImage(images.get(progress), point.x, point.y, panel);
+					progress++;
+				} else {
+					progress = 0;
+					g.drawImage(images.get(progress), point.x, point.y, panel);
+				}
+			} else {
+				g.drawImage(images1.get(3), point.x, point.y, panel);//第三个图片为初始飞机
+			}
+			return true;
+		} 
+		return false;
 	}
 
 	public static boolean loadImage() {
 		try {
 			BufferedImage temp = ImageIO.read(new File("images/me.bmp"));
-			temp = ImageIO.read(new File("images/me1.bmp"));
 			temp = ImageUtil.createImageByMaskColorEx(temp, new Color(0, 0, 0));
-			for (int i = 0; i < 4; i++) {
-				BufferedImage image = temp.getSubimage(i * PLANE1_WIDTH, 0,
-						PLANE1_WIDTH, PLANE1_HEIGHT);
-				images1.add(image);
+			for (int i = 0; i < 14; i++) {
+				BufferedImage image = temp.getSubimage(i * PLANE_WIDTH, 0,
+						PLANE_WIDTH, PLANE_HEIGHT);
+				images.add(image);
 			}
 			temp = ImageIO.read(new File("images/me1.bmp"));
 			temp = ImageUtil.createImageByMaskColorEx(temp, new Color(0, 0, 0));
@@ -69,7 +101,7 @@ public class MyPlane extends GameObject {
 			BufferedImage image = ImageIO.read(new File("images/protect.bmp"));
 			image = ImageUtil.createImageByMaskColorEx(image, new Color(255,
 					255, 255));
-			//imagespro.add(image);
+			imagespro.add(image);
 			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -80,6 +112,14 @@ public class MyPlane extends GameObject {
 	@Override
 	public Rectangle getRect() {
 		return new Rectangle(point.x, point.y, PLANE_WIDTH, PLANE_HEIGHT);
+	}
+
+	public boolean isUpdate() {
+		return isUpdate;
+	}
+
+	public void setUpdate(boolean isUpdate) {
+		this.isUpdate = isUpdate;
 	}
 
 	public int getProgress() {
